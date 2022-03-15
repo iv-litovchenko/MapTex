f5edccd46d2e582937b9188cac2d5e53f13d720a
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-03-15 17:18:37 +0300

    Начал содание класса сервиса
    и валидации ошибок

--- app/Http/Controllers/Admin/Technologies/EditController.php
+++ app/Http/Controllers/Admin/Technologies/EditController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin\Technologies;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
