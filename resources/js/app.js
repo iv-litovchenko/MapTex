0a389d36396757c4039a7f846e2417b31d7aebe7
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-03-30 17:24:33 +0300

    Ищем баг (2)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,22 @@
 global.jquery = global.jQuery = global.$ = require('jquery/dist/jquery');
 const Bootstrap = require('bootstrap/dist/js/bootstrap');
+hljs = require('highlight.js/lib/common'); // Highlight.Js
+
+$(document).ready(function () {
+
+    // Подсветка синтаксиса
+    // html = hljs.highlight('<h1>Hello World!</h1>', {language: 'xml'}).value
+    $("pre[class^='language']").each(function () {
+        var content = $(this).text();
+        var codeLanguage = $(this).attr('class');
+        codeLanguage = codeLanguage.replace('language-', '');
+
+        var hljsContent = hljs.highlight(content, {language: codeLanguage}).value;
+        $(this).html(hljsContent);
+        $(this).css('border','none');
+        $(this).css('border-radius','none');
+        // alert(codeLanguage + ' : ' + hljsContent);
+    });
+
+});
 
