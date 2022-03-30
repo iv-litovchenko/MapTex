a724e9052918299b7a231e05f13e17daea944958
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-03-30 17:05:52 +0300

    Ищем баг...

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,3 @@
 global.jquery = global.jQuery = global.$ = require('jquery/dist/jquery');
 const Bootstrap = require('bootstrap/dist/js/bootstrap');
-const TinyMCE = require('tinymce/tinymce');
-
-require('tinymce/icons/default');
-require('tinymce/models/dom');
-require('tinymce/themes/silver');
-require('tinymce/plugins/advlist');
-require('tinymce/plugins/autolink');
-require('tinymce/plugins/lists');
-require('tinymce/plugins/link');
-require('tinymce/plugins/anchor');
-require('tinymce/plugins/table');
-require('tinymce/plugins/code');
-require('tinymce/plugins/codesample');
-hljs = require('highlight.js/lib/common'); // Highlight.Js
-
 
