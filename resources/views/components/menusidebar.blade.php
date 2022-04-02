98688809d311c6b31031d62217449c940693c145
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-04-02 18:54:05 +0300

    Тестируем меню

--- resources/views/components/menusidebar.blade.php
+++ resources/views/components/menusidebar.blade.php
@@ -2,23 +2,25 @@
 @if(count($rows)>0)
     <ul class="{{ $htmlUlClass }}">
         @foreach($rows as $row)
-            <li @if($isActive($row->id,  $currentPostId)) class="active" @endif>
+            <li @if($isActive($row->id, $currentPostId)) class="active" @endif>
                 <a href="{{ route('site.post', $row->id) }}">
+                    &raquo;
                     @if($row->logo_image)
                         <img src="{{ asset('storage/site/post/logo/'.$row->logo_image) }}" height="14">
-                @endif
-                {{--                    @auth--}}
-                {{--                        #{{ $row->id }} |--}}
-                {{--                    @endauth--}}
-                {{ Str::limit($row->name, 32) }}
-                {{--                    <span class="badge">1,118</span>--}}
-                <!-- <b class="caret"></b> -->
+                    @endif
+                    @auth
+                        #{{ $row->id }} |
+                    @endauth
+                    {{ Str::limit($row->name, 32) }}
+                    {{-- <span class="badge">1,118</span> <b class="caret"></b>--}}
                 </a>
-                <x-menu-sidebar
-                    parent-id="{{ $row->id }}"
-                    current-post-id="{{ $currentPostId }}"
-                    html-ul-class="dropdown-menu menu-sidebar-level-next"
-                />
+                @if($isActive($row->id, $currentPostId))
+                    <x-menu-sidebar
+                        parent-id="{{ $row->id }}"
+                        current-post-id="{{ $currentPostId }}"
+                        html-ul-class="dropdown-menu menu-sidebar-level-next"
+                    />
+                @endif
             </li>
             <!-- <li class="divider"></li> -->
             <!-- <li class="dropdown-header">Nav header</li> -->
