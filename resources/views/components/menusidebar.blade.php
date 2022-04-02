80db700aacf99d1ef87d078fd7bba38fed8842ca
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-04-02 17:47:28 +0300

    Тестим меню

--- resources/views/components/menusidebar.blade.php
+++ resources/views/components/menusidebar.blade.php
@@ -2,7 +2,16 @@
     <ul class="{{ $htmlUlClass }}">
         @foreach($rows as $row)
             <li>  <!--class="active"-->
-                <a href="#">{{ $row->name }} <span class="badge">1,118</span></a>
+                <a href="{{ route('site.post', $row->id) }}">
+                    @if($row->logo_image)
+                        <img src="{{ asset('storage/site/post/logo/'.$row->logo_image) }}" height="32">
+                    @endif
+                    @auth
+                        #{{ $row->id }} |
+                    @endauth
+                    {{ Str::limit($row->name, 32) }}
+                    <span class="badge">1,118</span>
+                </a>
                 <x-menu-sidebar parent-id="{{ $row->id }}" html-ul-class="dropdown-menu menu-sidebar-level-next"/>
             </li>
         @endforeach
