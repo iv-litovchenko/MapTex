4d064eb301a9da84eb630bc410261c5487d5a4ce
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-04-05 16:25:59 +0300

    Правка картинок

--- resources/views/components/post-content-type/partials/content.blade.php
+++ resources/views/components/post-content-type/partials/content.blade.php
@@ -1,6 +1,6 @@
 @if($post->fiagma_image)
 
-    <img src="{{ asset('storage/site/post/figma/'.$post->figma_image) }}" class="img-thumbnail" style="width: 100%;">
+    <img src="{{ asset('storage/'.$post->figma_image) }}" class="img-thumbnail" style="width: 100%;">
     <hr class="my-12">
 
 @endif
