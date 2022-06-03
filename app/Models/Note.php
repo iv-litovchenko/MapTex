33e9a0e048481eb1740d09cd520c7bac74f04b20
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-06-03 16:01:27 +0300

    fix

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * App\Models\Note
