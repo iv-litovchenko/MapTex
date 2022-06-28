a81e9014ca08415b5add91ff7548bef261fc91bb
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-06-28 14:02:42 +0300

    Fix bug js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js').vue().version(); // div id="app"
+mix.js('resources/js/app.js', 'public/js').version(); // div id="app"
 mix.css('resources/css/app.css', 'public/css').version();
 // mix.sass('resources/sass/app.scss', 'public/css');
 
