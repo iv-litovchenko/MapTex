4f2f68220847c1dd823a3c4aa1a2b1688de53f33
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-09-12 13:54:33 +0300

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -34,7 +34,7 @@ class SiteController extends BaseController
         // $postTodo = Post::find(72);
         // $lastNote = Note::where('note_type', Note::NOTE_TYPE_POST_COMMENT)->orderBy('id', 'desc')->first();
 
-        $todos = Todo::orderBy('created_at', 'desc')->get();
+        $todos = Todo::where('is_close', 1)->orderBy('created_at', 'desc')->get();
         $todoHttpLink = 'https://raw.githubusercontent.com/iv-litovchenko/maptex/master/README.md';
         return view('site.home', compact(
                 'posts',
