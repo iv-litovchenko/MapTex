42c0c6dfd638d1709fefb953ecb4ee661565500c
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-09-15 10:42:15 +0300

    Deploy (2)

--- .bush/deploy.sh
+++ .bush/deploy.sh
@@ -2,20 +2,20 @@
 #set -x
 #cd /home/forge/domain.com
 #cd $FORGE_SITE_PATH
-#$PHP_PATH artisan down || true
+$PHP_PATH artisan down || true
 #git reset --hard
 #git clean -df
-#git pull origin $BRANCH
+git pull origin $BRANCH
 #composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 #npm ci
 #npm install
 #npm run production
-#$PHP_PATH artisan optimize
-#$PHP_PATH artisan migrate:auto --force
-#$PHP_PATH artisan cache:clear
-#$PHP_PATH artisan auth:clear-resets
-#$PHP_PATH artisan route:cache
-#$PHP_PATH artisan config:cache
-#$PHP_PATH artisan view:cache
-#$PHP_PATH artisan up
+$PHP_PATH artisan optimize
+$PHP_PATH artisan migrate:auto --force
+$PHP_PATH artisan cache:clear
+$PHP_PATH artisan auth:clear-resets
+$PHP_PATH artisan route:cache
+$PHP_PATH artisan config:cache
+$PHP_PATH artisan view:cache
+$PHP_PATH artisan up
 echo "Successful deployment [sh]!";
