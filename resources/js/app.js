c18dee1345c6f41ceab05752a62d043eaa1f068e
Ivan Litovchenko <iv-litovchenko@mail.ru>
2022-09-27 13:40:27 +0300

    master: Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ require('bootstrap/dist/js/bootstrap'); // Bootstrap
 
 const hljs = require('highlight.js/lib/common'); // Highlight.Js
 const tinymce = require('tinymce/tinymce'); // TinyMCE
+const Vue = require('vue');
 
 require('tinymce/icons/default');
 require('tinymce/models/dom');
@@ -79,7 +80,6 @@ $("pre[class^='language']").each(function () {
 //     return $(this).html().replace(new RegExp(qSearchValue + "(?=[^>]*<)", "ig"), "<span class='search-sot'>$&</span>");
 // });
 
-window.Vue = require('vue');
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 const app = new Vue({
     el: '#app',
