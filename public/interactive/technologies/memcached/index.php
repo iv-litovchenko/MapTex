f3f03c213a680abcef0f76a311f3f68b05a0296d
misc1170 <sloopy1170@gmail.com>
2023-10-23 18:15:47 +0300

    Memcached & Redis (index.php)

--- public/interactive/technologies/memcached/index.php
+++ public/interactive/technologies/memcached/index.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'Hi memcached';
+$mem_var = new \Memcached();
+$mem_var->addServer("maptex-db-memcached", 11211);
+$response = $mem_var->get("Bilbo");
+
+$version = $mem_var->getVersion();
+echo "Server's version: ".print_r($version)."<br/>\n";
+
+
+$key = "GEEKSFORGEEKS";
+$value = "computer science portal";
+$ttl = 3600;
+if ($mem_var->add($key, $value, $ttl))
+    echo "** added key-value (" . $key . ":"
+        . $value . ")to cache successfully!! **\n";
+else
+    echo "** error while adding value to cache!! **\n";
+
+// Get value of key
+echo "****   FETCHED VALUE   FOR KEY :"
+    . $key . " ****\n";
+
+$valD = $mem_var->get($key);
+var_dump($valD);
