25c6ab4f4add69c5168243b97a58a2a379e1675d
misc1170 <sloopy1170@gmail.com>
2023-10-24 17:51:43 +0300

    Deploy update

--- .bash/deploy.sh
+++ .bash/deploy.sh
@@ -14,24 +14,28 @@ echo "Start deployment [sh]!";
 #sudo /usr/bin/supervisorctl update
 #sudo /usr/bin/supervisorctl stop laravel-worker:*
 
-$PHP_PATH artisan down
-#git reset --hard
-#git clean -df
-git pull origin $BRANCH
-#composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
-#composer dump-autoload
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan down
+
+git pull origin master
+
+docker exec  -w /var/www/html -it maptex-web-php-fpm composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+docker exec  -w /var/www/html -it maptex-web-php-fpm composer dump-autoload
+
 #npm ci
 #npm install
 #npm run production
-$PHP_PATH artisan migrate:auto --force
-$PHP_PATH artisan cache:clear
-$PHP_PATH artisan auth:clear-resets
-$PHP_PATH artisan route:clear
-$PHP_PATH artisan route:cache
-$PHP_PATH artisan config:clear
-$PHP_PATH artisan config:cache
-$PHP_PATH artisan view:clear
-$PHP_PATH artisan view:cache
+
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan migrate:auto --force
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan cache:clear
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan auth:clear-resets
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan route:clear
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan route:cache
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan config:clear
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan config:cache
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan view:clear
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan view:cache
+
 # php artisan optimize
-$PHP_PATH artisan up
+docker exec  -w /var/www/html -it maptex-web-php-fpm php artisan up
+
 echo "End deployment [sh]!";
