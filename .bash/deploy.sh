1e20cad30914841274a19936dee0c899e5a5ad18
misc1170 <sloopy1170@gmail.com>
2023-10-26 13:17:42 +0300

    Test

--- .bash/deploy.sh
+++ .bash/deploy.sh
@@ -15,26 +15,26 @@ echo "Start deployment [sh]!"
 #sudo /usr/bin/supervisorctl update
 #sudo /usr/bin/supervisorctl stop laravel-worker:*
 
-php artisan down
+php artisan down;
 
-git pull origin master
+git pull origin master;
 
-composer install
-composer dump-autoload
+composer install;
+composer dump-autoload;
 
 # npm install;
 # npm run production;
 
 # php artisan migrate:auto --force
-php artisan cache:clear
-php artisan auth:clear-resets
-php artisan route:clear
-php artisan route:cache
-php artisan config:clear
-php artisan config:cache
-php artisan view:clear
-php artisan view:cache
-php artisan optimize
+php artisan cache:clear;
+php artisan auth:clear-resets;
+php artisan route:clear;
+php artisan route:cache;
+php artisan config:clear;
+php artisan config:cache;
+php artisan view:clear;
+php artisan view:cache;
+php artisan optimize;
 php artisan up
 
-echo "End deployment [sh]!"
+echo "End deployment [sh]!";
