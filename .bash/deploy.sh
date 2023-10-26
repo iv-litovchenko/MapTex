b201ff6dd38a1b0202d6a2b68dbeddb7de52fa26
misc1170 <sloopy1170@gmail.com>
2023-10-26 13:18:44 +0300

    Test

--- .bash/deploy.sh
+++ .bash/deploy.sh
@@ -19,22 +19,3 @@ echo "Start deployment [sh]!";
 
 git pull origin master;
 
-composer install;
-composer dump-autoload;
-
-# npm install;
-# npm run production;
-
-# php artisan migrate:auto --force
-php artisan cache:clear;
-php artisan auth:clear-resets;
-php artisan route:clear;
-php artisan route:cache;
-php artisan config:clear;
-php artisan config:cache;
-php artisan view:clear;
-php artisan view:cache;
-php artisan optimize;
-php artisan up
-
-echo "End deployment [sh]!";
