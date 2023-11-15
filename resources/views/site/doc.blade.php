101059a769b233cb89a8fd360e0504acfcffe779
misc1170 <sloopy1170@gmail.com>
2023-11-15 16:15:08 +0300

    Doc 6

--- resources/views/site/doc.blade.php
+++ resources/views/site/doc.blade.php
@@ -38,6 +38,16 @@
                         <ul class="nav navbar-nav">
                             @foreach(\App\Models\Doc::getCategories() as $key => $name)
                                 <li @if($key == $cat) class="active" @endif>
+                                    <img
+                                            src="{{ asset('assets/images/folder.png') }}"
+                                            height="20"
+                                            style="
+                                                    vertical-align: top;
+                                                    padding: 0px;
+                                                    border-radius: 0%;
+                                                    border: gray 0px solid;
+                                                    "
+                                    />
                                     <a href="{{ route('site.doc-cat', $key) }}">{{ $key }}. {{ $name }}</a>
                                 </li>
                             @endforeach
