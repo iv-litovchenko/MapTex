7b04954722442a75a8419bceef1c7bfee393a44b
macbook <iv-litovchenko@mail.ru>
2025-04-16 18:31:48 +0300

    Делаем защищенный пост (страницу)

--- resources/views/components/post-content-type/page-protected.blade.php
+++ resources/views/components/post-content-type/page-protected.blade.php
@@ -1,1 +1,9 @@
+@extends('layouts.default')
+
+@section('LayoutSectionPageTitle', 'В доступе отказано')
+@section('LayoutSectionPageHeader', 'В доступе отказано')
+@section('LayoutSectionPageBreadcrumb', Breadcrumbs::render('site.post', $post))
+
+@section('LayoutSectionPageContent')
 fewkjfewklfewkml
+@endsection
