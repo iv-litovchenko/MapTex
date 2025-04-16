0b2a5dfd39b4e818300e237bdde1389acad63dc8
macbook <iv-litovchenko@mail.ru>
2025-04-16 15:35:12 +0300

    Правка интерактива

--- resources/views/components/post-content-type/partials/content.blade.php
+++ resources/views/components/post-content-type/partials/content.blade.php
@@ -17,8 +17,6 @@
 @if($post->maptex_content_link)
 
     {{-- Контент --}}
-    <pre class="language-xml">{!! nl2br(file_get_contents(public_path('/interactive/content_wiki/' . $post->maptex_content_link))) !!}</pre>
-    <pre class="language-xml">/interactive/content_wiki/{{ $post->maptex_content_link }}</pre>
-    <hr class="my-12">
+   {{ $post->maptex_content_link }}
 
 @endif
